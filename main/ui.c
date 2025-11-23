db3760ff90f842e2d53fdab0995acdecd815710d
zulin <9683884@qq.com>
2025-11-23 09:13:57 +0800

    OK. LVGL可以, 不要lvglport了

--- main/ui.c
+++ main/ui.c
@@ -18,10 +18,16 @@ esp_err_t ui_init(void)
     lv_obj_set_style_text_color(label, lv_color_make(0xFF, 0xFF, 0xFF), 0);
     lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 5);
     
+    // 创建灰色文字
+    lv_obj_t *label2 = lv_label_create(scr);
+    lv_label_set_text(label2, "Gray Text Line");
+    lv_obj_set_style_text_color(label2, lv_color_make(0x88, 0x88, 0x88), 0);
+    lv_obj_align(label2, LV_ALIGN_TOP_MID, 0, 25);
+    
     // 创建进度条 - 灰底白条
     lv_obj_t *bar = lv_bar_create(scr);
     lv_obj_set_size(bar, 200, 10);
-    lv_obj_align(bar, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_align(bar, LV_ALIGN_CENTER, 0, 10);
     lv_obj_set_style_bg_color(bar, lv_color_make(0x33, 0x33, 0x33), LV_PART_MAIN);
     lv_obj_set_style_bg_color(bar, lv_color_make(0xFF, 0xFF, 0xFF), LV_PART_INDICATOR);
     lv_bar_set_value(bar, 75, LV_ANIM_OFF);
